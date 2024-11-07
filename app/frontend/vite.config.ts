7a56d4d30eabf25032de2a42c213a49a4a9a3e23
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2024-11-07 15:41:31 -0300

    fix(): vite.config

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+    build:{
+    outDir:'dist',
+    sourcemap: false
+  },
+  server:{
+    port: 5001,
+    open: true,
+  },
+  base: '/inventario',
 })
