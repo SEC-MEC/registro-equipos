5e0eb8cbfd332da17fa4cca633dfb2d906b1e44d
Lucio B. Schiavoni Lafourcade <lucio.schiavoni@mec.local>
2024-12-11 12:01:49 -0300

    fix(): fixProd

--- app/frontend/src/components/form/UserForm.tsx
+++ app/frontend/src/components/form/UserForm.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardFooter } from '@/components/ui/card';
+import { Card } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useForm } from 'react-hook-form';
