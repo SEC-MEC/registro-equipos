176ec277b0b210fa67bc6a31658a8ad627542d60
Lucio B. Schiavoni Lafourcade <lucio.schiavoni@mec.local>
2024-12-12 08:14:04 -0300

    esModule tailwind

--- app/frontend/tailwind.config.js
+++ app/frontend/tailwind.config.js
@@ -1,40 +1,36 @@
 /** @type {import('tailwindcss').Config} */
-const { nextui } = require("@nextui-org/react");
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
+import { nextui } from "@nextui-org/react";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
-module.exports = {
-  darkMode: ["class"],
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {},
-      animation: {
-        aurora: "aurora 60s linear infinite",
-      },
-      keyframes: {
-        aurora: {
-          from: {
-            backgroundPosition: "50% 50%, 50% 50%",
-          },
-          to: {
-            backgroundPosition: "350% 50%, 350% 50%",
-          },
+export const darkMode = ["class"];
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+];
+export const theme = {
+  extend: {
+    colors: {},
+    animation: {
+      aurora: "aurora 60s linear infinite",
+    },
+    keyframes: {
+      aurora: {
+        from: {
+          backgroundPosition: "50% 50%, 50% 50%",
+        },
+        to: {
+          backgroundPosition: "350% 50%, 350% 50%",
         },
       },
-    }
-  },
-  plugins: [
-    require("tailwindcss-animate"),
-    nextui(),
-    addVariablesForColors
-  ]
+    },
+  }
 };
+export const plugins = [
+  require("tailwindcss-animate"),
+  nextui(),
+  addVariablesForColors
+];
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
 function addVariablesForColors({ addBase, theme }) {
